3b879a431f97dc262d512b1c1aa4f688f2357965
CactusTribe <lefrancjoaquim@gmail.com>
2017-03-11 13:05:23 +0100

    Mise a jour de l'arbo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-package sample;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,7 +7,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
